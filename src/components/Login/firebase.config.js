c32e27d56ff260612727938766f7c8fd6b1b364c
mohammadshahed <silentshahed@gmail.com>
2020-10-01 21:34:45 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAa2wjTTNUFr7q30BB5f5qxh8Wu1zNAUjs",
+    authDomain: "burj-aal-aarab.firebaseapp.com",
+    databaseURL: "https://burj-aal-aarab.firebaseio.com",
+    projectId: "burj-aal-aarab",
+    storageBucket: "burj-aal-aarab.appspot.com",
+    messagingSenderId: "478788990756",
+    appId: "1:478788990756:web:4b2acfce1ad9f682d5a5b1"
+  };
+  export default firebaseConfig;
